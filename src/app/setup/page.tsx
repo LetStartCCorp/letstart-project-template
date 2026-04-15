182d336b403ada57f9313e4a2a9ffc90f9dba04e
Gürkan ARKAŞ <gurkanarkas@gmail.com>
2026-04-16 00:59:03 +0300

    feat: add setup wizard page

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+export default function SetupPage() {
+  const html = fs.readFileSync(
+    path.join(process.cwd(), "node_modules", "@letstart", "setup", "wizard.html"),
+    "utf-8",
+  );
+
+  return (
+    <html lang="en" className="dark">
+      <body dangerouslySetInnerHTML={{ __html: html }} />
+    </html>
+  );
+}
